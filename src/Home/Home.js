2fc0020af98b7c84539a061f748c104281b4de60
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2020-01-27 15:41:41 -0700

    Render skeleton footer component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './../Header/Header';
+import Footer from './../Footer/Footer';
 import './Home.scss';
 
 function Home() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   )
 }
