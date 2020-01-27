f2d13f3a0f79bd4939bde59d5c094552cfdefa79
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2020-01-27 15:05:44 -0700

    Set up redux

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RecipeFrom from './../RecipeForm/RecipeForm'
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <RecipeFrom />
+    </div>
+  );
+}
+
+export default App;
