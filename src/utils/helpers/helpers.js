17015d84d36393dd7118b6d4354658ea03bb4d01
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2020-02-21 11:33:55 -0700

    Add method with logic to rename keys in store

--- src/utils/helpers/helpers.js
+++ src/utils/helpers/helpers.js
@@ -4,6 +4,7 @@ export const displayRandomRecipes = (recipes) => {
 };
 
 export const getCategoryIds = recipes => {
+  let renamedRecipes = renameCategories(recipes)
   return recipes.reduce((acc, recipe) => {
     if (!acc[recipe.category]){
       acc[recipe.category] = recipe.category_id
@@ -11,6 +12,22 @@ export const getCategoryIds = recipes => {
 
     return acc;
   }, {})
+}
+
+const renameCategories = recipes => {
+  recipes.map(recipe => {
+    if (recipe.category === 'Meals') {
+      recipe.category = 'meals'
+    } else if (recipe.category === 'Soups and Salads') {
+      recipe.category = 'soups_and_salads'
+    } else if (recipe.category === 'Apps') {
+      recipe.category = 'apps'
+    } else if (recipe.category === 'Desserts') {
+      recipe.category = 'desserts'
+    } else if (recipe.category === 'Beverages'){
+      recipe.category = 'beverages'
+    }
+  })
 }
 
 
