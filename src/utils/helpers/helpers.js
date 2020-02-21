dd96448d0f75fb20f6b70be2e4e1e4a6621ae2d2
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2020-02-21 10:56:56 -0700

    Edit typos

--- src/utils/helpers/helpers.js
+++ src/utils/helpers/helpers.js
@@ -1,4 +1,9 @@
 export const displayRandomRecipes = (recipes) => {
   let randomRecipes = recipes.splice(0, 6)
   return randomRecipes
-}
+};
+
+//export const getCategoryIds = recipes => {
+  // (already have the names of the category so hard code this!)
+  //return //=> {category_1: 1, category_2:2}
+//}
