20ea747fd30f49e160b6f7cf24374d668411d734
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2020-02-21 12:11:32 -0700

    Fetch recipes by category when button in header is clicked

---
+++ src/containers/Header/Header.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getRecipesByCategory } from './../../utils/apiCalls/apiCalls';
+import { selectRecipes } from './../../actions/index';
+
+import './Header.scss';
+
+export class Header extends Component {
+
+  updateRecipesDisplayed = async (category) => {
+    const { categoryIds, selectRecipes } = this.props;
+    let recipes = await getRecipesByCategory(categoryIds[category])
+
+
+    console.log(recipes)
+  }
+
+  render () {
+
+    return (
+      <div className="Header">
+        <h1>Evans<span>Eats</span></h1>
+        <div>
+          <h3>+</h3>
+          <ul>
+            <Link to='/category'>
+              <li onClick={() => this.updateRecipesDisplayed('apps')}>Apps</li>
+            </Link>
+            <Link to='/category'>
+              <li onClick={() => this.updateRecipesDisplayed('soups_and_salads')}>Soups and Salads</li>
+            </Link>
+            <Link to='/category'>
+              <li onClick={() => this.updateRecipesDisplayed('meals')}>Meals</li>
+            </Link>
+            <Link to='/category'>
+              <li onClick={() => this.updateRecipesDisplayed('beverages')}>Beverages</li>
+            </Link>
+            <Link to='/category'>
+              <li onClick={() => this.updateRecipesDisplayed('desserts')}>Desserts</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+  }
+
+
+export const mapDispatchToProps = dispatch => ({
+  selectRecipes: recipesSelected => dispatch(selectRecipes(recipesSelected)),
+  }
+)
+
+export const mapStateToProps = state => ({
+  categoryIds: state.categoryIds
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
