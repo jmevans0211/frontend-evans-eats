9e9a0e0d7cc7da111845d30c78596e5fdfe4a421
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2020-02-27 11:26:00 -0700

    Overlap recipe with image adding opacity to background color of the h3

--- src/RecipeCard/RecipeCard.js
+++ src/RecipeCard/RecipeCard.js
@@ -4,8 +4,8 @@ import './RecipeCard.scss'
 function RecipeCard({ recipe_name, image_url}) {
   return (
     <div className='recipeCard__div'>
-      <h4>{recipe_name}</h4>
-      <img src={image_url} alt={recipe_name} style ={{width: '300px'}}/>
+      <h4 className='recipeCard__div--h4'>{recipe_name}</h4>
+      <img className='recipeCard__div--img' src={image_url} alt={recipe_name} style ={{width: '300px'}}/>
     </div>
   )
 }
