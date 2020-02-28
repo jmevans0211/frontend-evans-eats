8c0179d0c6323a72d2563c665d34c5fba206d964
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2020-02-27 20:29:25 -0700

    Add navigation links to recipe cards

---
+++ src/Recipe/Recipe.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Header from '../containers/Header/Header';
+import Footer from '../Footer/Footer';
+
+export class Recipe extends Component {
+  render() {
+    return (
+      <p>in recipe component!!!!</p>
+    )
+  }
+}
--- src/containers/CategoryDisplay/CategoryDisplay.js
+++ src/containers/CategoryDisplay/CategoryDisplay.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Header from '../Header/Header';
 import Footer from '../../Footer/Footer';
@@ -10,7 +11,16 @@ export class CategoryDisplay extends Component {
   render() {
     let { recipesSelected } = this.props
     let recipeCards = recipesSelected.map(recipe => {
-      return <RecipeCard key={recipe.recipe_name} recipe_name={recipe.recipe_name} image_url={recipe.image_url}/>
+      return (
+        <Link to='/recipe' className='routerLink'>
+          <RecipeCard 
+            key={recipe.recipe_name}
+            recipe_name={recipe.recipe_name} 
+            image_url={recipe.image_url}
+            
+          />
+        </Link>
+      )
     })
 
     return (
@@ -31,6 +41,3 @@ export const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps, null)(CategoryDisplay);
-
-// {recipesSelected.length <= 0 && <img src='https://media.giphy.com/media/3o7bu8sRnYpTOG1p8k/giphy.gif'/>}
-
